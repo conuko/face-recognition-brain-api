2e0f7df15cfc539f2696d390b8155b66ceae026e
Constantin <constantinu93@gmail.com>
2020-11-15 14:18:03 +0100

    add /profile and /image

--- server.js
+++ server.js
@@ -30,9 +30,6 @@ app.get('/', (req, res) => {
     res.send(database.users);
 })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
-});
 
 // /SIGN IN --> check if the user typed in on the frontend singin is already in the database
 app.post('/signin', (req, res) => {
@@ -59,12 +56,37 @@ app.post('/register', (req, res) => {
     res.json(database.users[database.users.length-1]);
 });
 
+// PROFILE/:USER ID
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
 
-/* 
-/--> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user 
+// IMAGE --> count how many entries a user has
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
 
-*/
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+});
